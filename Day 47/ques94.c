2aed5b38f880a62ec6917995a73541bcf2e22598
Abhiraj <abhirajsinghrohila@gmail.com>
2025-10-25 21:58:20 +0530

    main

---
+++ Day 47/ques94.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+
+int main() {
+    char str[200], word[50], longest[50];
+    int i = 0, j = 0, maxLen = 0, len = 0;
+
+    printf("Enter a sentence: ");
+    gets(str);  
+
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && str[i] != '\n' && str[i] != '\t') {
+            word[j++] = str[i];
+        } else {
+            word[j] = '\0';  
+            len = strlen(word);
+            if (len > maxLen) {
+                maxLen = len;
+                strcpy(longest, word);
+            }
+            j = 0;  
+        }
+        i++;
+    }
+
+    
+    word[j] = '\0';
+    len = strlen(word);
+    if (len > maxLen) {
+        maxLen = len;
+        strcpy(longest, word);
+    }
+
+    printf("The longest word is: %s", longest);
+    printf("\nLength: %d", maxLen);
+
+    return 0;
+}
