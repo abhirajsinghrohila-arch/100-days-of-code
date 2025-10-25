0f308814ae4aea28f0d320bb844fb247eaef498f
Abhiraj <abhirajsinghrohila@gmail.com>
2025-10-25 21:41:33 +0530

    main

---
+++ Day 46/ques92.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0};  
+    int i, found = 0;
+
+    printf("Enter a string: ");
+    gets(str);  
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            freq[str[i] - 'a']++;
+
+            if(freq[str[i] - 'a'] == 2) {  
+                printf("First repeating lowercase alphabet: %c", str[i]);
+                found = 1;
+                break;
+            }
+        }
+    }
+
+    if(!found)
+        printf("No repeating lowercase alphabet found.");
+
+    return 0;
+}
