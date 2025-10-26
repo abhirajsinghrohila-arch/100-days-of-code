430eff86ec627aae74224a1e9e55d4a90f045c93
Abhiraj <abhirajsinghrohila@gmail.com>
2025-10-26 14:30:48 +0530

    main

---
+++ Day 49/ques97.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>  
+
+int main() {
+    char name[100];
+    int i;
+
+    printf("Enter your full name: ");
+    fgets(name, sizeof(name), stdin);  
+
+    printf("Initials: ");
+    
+    if (isalpha(name[0])) {
+        printf("%c ", toupper(name[0]));
+    }
+
+    for (i = 0; name[i] != '\0'; i++) {
+        
+        if (name[i] == ' ' && isalpha(name[i + 1])) {
+            printf("%c ", toupper(name[i + 1]));
+        }
+    }
+
+    return 0;
+}
